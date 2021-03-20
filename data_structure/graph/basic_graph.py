87827f2e14a230096e790da27f6cb225de9df8e3
Long-yuyao <law15872183526@hotmail.com>
2021-03-20 23:13:07 +0800

    stack

---
+++ data_structure/graph/basic_graph.py
@@ -0,0 +1,7 @@
+class Graph:
+    def __init__(self, vertex):
+        self.vertex = vertex
+        self.edge = []
+
+    def add_edge(self, edge):
+        self.edge.append(edge)
---
+++ data_structure/stack/largest_area.py
@@ -0,0 +1,25 @@
+"""
+input:[2,1,5,6,2,3]
+output:10
+take place heights with orders
+"""
+
+
+class Solution:
+    def largestRectangleArea(self, heights: [int]) -> int:
+        n = len(heights)
+        left = [0] * n
+        right = [n] * n
+        stack = []
+        for i in range(n):
+            while stack and heights[i] <= heights[stack[-1]]:
+                right[stack[-1]] = i
+                stack.pop()
+            left[i] = stack[-1] + 1 if stack else 0
+            stack.append(i)
+
+        return max(((right[i]-left[i]) * heights[i]) for i in range(n))
+
+
+if __name__ == '__main__':
+    assert Solution().largestRectangleArea([4, 2, 0, 3, 2, 4, 3, 4]) == 10
